aab25633fb760389be0108bac25cad629bb32c40
TazzCN <tazzcn@gmail.com>
2019-10-29 09:49:29 +0000

    Section 2: Building Content with JSX

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 
 const App = () => {
-    return <div>Hi There!</div>;
+    const buttonText = 'Click Me';
+    const style = {backgroundColor: 'blue', color: 'white'};
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={style}>
+                {buttonText}
+            </button>
+        </div>
+    );
 };
 
 ReactDOM.render(
